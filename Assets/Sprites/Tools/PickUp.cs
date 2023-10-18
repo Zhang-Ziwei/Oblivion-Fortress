2adc16153abfbd4bab86860158008d903e77e2fc
Zhang-Ziwei <weiweizhang531@gmail.com>
2023-10-18 12:20:40 +0800

    pick up system

---
+++ Assets/Sprites/Tools/PickUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        UnityEngine.Debug.Log("³ÖÐøÅö×²:");
+    }
+
+}
