5ba61a2b906e7a3e00cf69a2c83926477bb496fd
henryhuang920712 <91250123+henryhuang920712@users.noreply.github.com>
2023-10-30 13:43:32 +0800

    10/30 Enemies

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+
 //using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,15 +13,30 @@ public class Enemy : MonoBehaviour
     private float health;
     public float damage;
     public float speed;
+
+    public float attackRange;
+
+    public float detectRange;
+
     private int ID;
 
     public Slider healthBar;
 
-    private Vector3 DefaultDirection = new Vector3(-1, -0.5f, 0).normalized;
-
     private int PathIndex;
 
-    private Transform target;
+    private Transform nextPath;
+
+    private GameObject player;
+
+    private Transform playerGround;
+
+    private int actionMode;
+
+    private Animator animator;
+
+    private string isAttacking = "IsAttacking";
+    private string isAttacked = "IsAttacked";
+    private string isDead = "IsDead";
 
     public void Init(int enemyID)
     {
@@ -28,33 +45,79 @@ public class Enemy : MonoBehaviour
         ID = enemyID;
         healthBar.value = health / maxHealth;
 
+        actionMode = 1;
+
+        // search for the player
+        player = GameObject.Find("Player");
+
+        // playerground is the transform of the player's child when name "GroundSensor"
+        playerGround = player.transform.Find("GroundSensor");
+
+        // get the animator in the first child
+        animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+
+        // set isAttacking, isAttacked, isDead to false
+        animator.SetBool(isAttacking, false);
+        animator.SetBool(isDead, false);
+
         // if maxhealth == 0, debug log
         if (maxHealth == 0)
         {
             Debug.Log("Enemy.cs: maxHealth is 0.");
         }
         
+        // if attack range > detect range, debug log
+        if (attackRange > detectRange)
+        {
+            Debug.Log("Enemy.cs: attackRange is larger than detectRange.");
+        }
+
         // set enemy to the first path location
         transform.position = LevelManager.Instance.PathLocations[0].position;
 
-        target = LevelManager.Instance.PathLocations[PathIndex];
+        nextPath = LevelManager.Instance.PathLocations[PathIndex];
     }
-
-    void Start()
-    {
-        // healthbar direction 
-        healthBar.transform.rotation = Quaternion.Euler(0, 0, 0);
-    }
-
     public int GetID() {
         return ID;
     }
 
-    void Update()
-    {
+    public void DeductHealth(float damage) {
+        health -= damage;
+        healthBar.value = health / maxHealth;
 
+        animator.SetTrigger(isAttacked);
+        // animator.Play("take_hit", -1, 0f);
+        if (health <= 0) {
+            StartCoroutine(DeathAnimation());
+        }
+    }
+
+    private IEnumerator DeathAnimation() {
+        animator.SetBool(isDead, true);
+        yield return new WaitForSeconds(1);
+        animator.SetBool(isDead, false);
+        EnemySummon.RemoveEnemy(this);
+    }
+
+    private void AssignNearestPath() {
+        // return the nearest path location in LevelManager.Instance.PathLocations
+        Transform nearestPath = LevelManager.Instance.PathLocations[0];
+        float minDistance = Vector3.Distance(transform.position, nearestPath.position);
+        int minIndex = 0;
+        for (int i = 1; i < LevelManager.Instance.PathLocations.Count; i ++) {
+            float distance = Vector3.Distance(transform.position, LevelManager.Instance.PathLocations[i].position);
+            if (distance < minDistance) {
+                nearestPath = LevelManager.Instance.PathLocations[i];
+                minDistance = distance;
+                minIndex = i;
+            }
+        }
+        PathIndex = minIndex;
+        nextPath = nearestPath;
+    }
+
+    private void Move(Transform target) {
         Vector2 direction = (target.position - transform.position).normalized;
-
         // if x is negative, flip the sprite of the child object
         GameObject Skin = transform.GetChild(0).gameObject;
         if (direction.x < 0)
@@ -65,12 +128,57 @@ public class Enemy : MonoBehaviour
         {
             Skin.GetComponent<SpriteRenderer>().flipX = false;
         }
-
-
-        // move towards the target
         transform.Translate(direction * speed * Time.deltaTime);
+    }
 
-        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+    private void AttackPlayer() {
+        if (actionMode == 0) {
+            // end chasing player
+            actionMode = -1;
+        } else if (actionMode == 1) {
+            // end moving to target
+            actionMode = -1;
+        }
+
+        Debug.Log("Enemy.cs: AttackPlayer() called.");
+
+        animator.SetBool(isAttacking, true);
+        // animator.SetTrigger(isAttacking);
+
+        // if (Vector3.Distance(transform.position, player.transform.position) <= attackRange) {
+        //     // deal damage to player
+        //     player.GetComponent<Player>().DeductHealth(damage);
+        // }
+
+        // if player is dead, end attacking
+        // if (player.GetComponent<Player>().GetHealth() <= 0) {
+        //     actionMode = 0;
+        // }
+    }
+
+    private void ChasePlayer() {
+        if (actionMode == 1) {
+            // end moving to target
+            actionMode = 0;
+        } else if (actionMode == -1) {
+            // end attacking
+            actionMode = 0;
+            animator.SetBool(isAttacking, false);
+
+        }
+        
+        Move(playerGround);
+    }
+
+    private void MoveToPath() {
+        if (actionMode == 0) {
+            // end chasing player
+            AssignNearestPath();
+            actionMode = 1;
+        }
+        Move(nextPath);
+
+        if (Vector3.Distance(transform.position, nextPath.position) <= 0.1f)
         {
             PathIndex++;
 
@@ -83,16 +191,36 @@ public class Enemy : MonoBehaviour
             }
             else
             {
-                target = LevelManager.Instance.PathLocations[PathIndex];
+                nextPath = LevelManager.Instance.PathLocations[PathIndex];
             }
         }
+    }
+
+    void Start()
+    {
+        // healthbar direction 
+        healthBar.transform.rotation = Quaternion.Euler(0, 0, 0);
+        // StartCoroutine(Test());
+    }
+
+    void Update()
+    {
+
+        if (player != null) {
+            if (Vector3.Distance(transform.position, playerGround.position) <= attackRange) {
+                AttackPlayer();
+            } else if (Vector3.Distance(transform.position, playerGround.position) <= detectRange) {
+                ChasePlayer();
+            } else {
+                MoveToPath();
+            }
+        }    
 
     }
-    public void DeductHealth(float damage) {
-        health -= damage;
-        healthBar.value = health / maxHealth;
-        if (health <= 0) {
-            EnemySummon.RemoveEnemy(this);
+    IEnumerator Test() {
+        while(true) {
+            yield return new WaitForSeconds(3);
+            DeductHealth(1);
         }
     }
 }
